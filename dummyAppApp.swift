a6dc8ae3db6d68354002bdb35d45ba091c81a085
DenysPashkov <30626869+DenysPashkov@users.noreply.github.com>
2024-04-25 14:20:45 +0200

    Initial commit

---
+++ dummyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  dummyAppApp.swift
+//  dummyApp
+//
+//  Created by denys pashkov on 25/04/24.
+//
+
+import SwiftUI
+
+@main
+struct dummyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
